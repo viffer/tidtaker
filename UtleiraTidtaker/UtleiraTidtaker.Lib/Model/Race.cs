ef2104c17581a7decaaad42ff1c92adac37d8ebd
Terje Olsøybakk <terje.olsoybakk@norconsult.com>
2018-06-13 08:01:00 +0200

    Simplified racelength Changed racekey

--- UtleiraTidtaker/UtleiraTidtaker.Lib/Model/Race.cs
+++ UtleiraTidtaker/UtleiraTidtaker.Lib/Model/Race.cs
@@ -38,6 +38,7 @@ namespace UtleiraTidtaker.Lib.Model
          * */
 
         private DateTime _raceTime;
+        private int raceLength = -1;
 
         public Race(string name, int year, bool male, DateTime raceday)
         {
@@ -93,32 +94,37 @@ namespace UtleiraTidtaker.Lib.Model
                 case "10 km":
                 case "10 km tineansatt":
                 case "10 km uil håndball g15":
-                    key = string.Format("{0}10KM", key);
+                    key = string.Format("10KM{0}", key);
                     _raceTime = new DateTime(raceday.Year, raceday.Month, raceday.Day, raceday.Hour + 1, raceday.Minute + 15, raceday.Second);
                     this.name = male ? "Menn 10km" : "Kvinner 10km";
+                    raceLength = 10000;
                     break;
                 case "5 km":
                 case "5 km tineansatt":
                 case "5 km uil håndball g15":
-                    key = string.Format("{0}{1}5KM", raceage, key);
+                    key = string.Format("5KM{0}{1}", key, raceage);
                     _raceTime = new DateTime(raceday.Year, raceday.Month, raceday.Day, raceday.Hour + 1, raceday.Minute + 30, raceday.Second);
-                    this.name = string.Format("{0} {1} {2} år", gender, this.name, raceagestring);
+                    this.name = string.Format("{0} 5km {1} år", gender, raceagestring);
+                    raceLength = 5000;
                     break;
                 case "5 km trim":
                     key = "TRIM";
                     _raceTime = new DateTime(raceday.Year, raceday.Month, raceday.Day, raceday.Hour + 1, raceday.Minute + 30, raceday.Second);
                     this.name = "Trimklasse uten tidtakning";
+                    raceLength = 4999;
                     break;
                 case "2 km barneløp 7-12":
                     key = "2KM";
                     _raceTime = new DateTime(raceday.Year, raceday.Month, raceday.Day, raceday.Hour, raceday.Minute + 15, raceday.Second);
                     this.name = "Barneløp 2 km, 7 - 12 år";
+                    raceLength = 2000;
                     break;
                 case "500m barneløp 4-6":
                 case "600m barneløp 3-6":
                     key = "600M";
                     _raceTime = new DateTime(raceday.Year, raceday.Month, raceday.Day, raceday.Hour, raceday.Minute, raceday.Second);
                     this.name = "Barneløp 600 meter, 3 - 6 år";
+                    raceLength = 600;
                     break;
                 default:
                     throw new Exception($"Illegal string: {this.name}");
@@ -143,72 +149,125 @@ namespace UtleiraTidtaker.Lib.Model
         {
             get
             {
-                switch (key.ToLowerInvariant())
-                {
-                    case "d10km":
-                    case "m10km":
-                    case "1d10km":
-                    case "2d10km":
-                    case "3d10km":
-                    case "4d10km":
-                    case "5d10km":
-                    case "6d10km":
-                    case "1m10km":
-                    case "2m10km":
-                    case "3m10km":
-                    case "4m10km":
-                    case "5m10km":
-                    case "6m10km":
-                    case "j10km":
-                    case "g10km":
-                    case "1j10km":
-                    case "2j10km":
-                    case "3j10km":
-                    case "4j10km":
-                    case "5j10km":
-                    case "6j10km":
-                    case "1g10km":
-                    case "2g10km":
-                    case "3g10km":
-                    case "4g10km":
-                    case "5g10km":
-                    case "6g10km":
-                        return 10000;
-                    case "1d5km":
-                    case "2d5km":
-                    case "3d5km":
-                    case "4d5km":
-                    case "5d5km":
-                    case "6d5km":
-                    case "1m5km":
-                    case "2m5km":
-                    case "3m5km":
-                    case "4m5km":
-                    case "5m5km":
-                    case "6m5km":
-                    case "1j5km":
-                    case "2j5km":
-                    case "3j5km":
-                    case "4j5km":
-                    case "5j5km":
-                    case "6j5km":
-                    case "1g5km":
-                    case "2g5km":
-                    case "3g5km":
-                    case "4g5km":
-                    case "5g5km":
-                    case "6g5km":
-                        return 5000;
-                    case "trim":
-                        return 4999;
-                    case "2km":
-                        return 2000;
-                    case "500m":
-                    case "600m":
-                        return 600;
-                    default:
-                        return -1;
-                }
+                //switch (key.ToLowerInvariant())
+                //{
+                //    case "d10km":
+                //    case "m10km":
+                //    case "1d10km":
+                //    case "2d10km":
+                //    case "3d10km":
+                //    case "4d10km":
+                //    case "5d10km":
+                //    case "6d10km":
+                //    case "1m10km":
+                //    case "2m10km":
+                //    case "3m10km":
+                //    case "4m10km":
+                //    case "5m10km":
+                //    case "6m10km":
+                //    case "j10km":
+                //    case "g10km":
+                //    case "1j10km":
+                //    case "2j10km":
+                //    case "3j10km":
+                //    case "4j10km":
+                //    case "5j10km":
+                //    case "6j10km":
+                //    case "1g10km":
+                //    case "2g10km":
+                //    case "3g10km":
+                //    case "4g10km":
+                //    case "5g10km":
+                //    case "6g10km":
+                //    case "10km d":
+                //    case "10km d1":
+                //    case "10km d2":
+                //    case "10km d3":
+                //    case "10km d4":
+                //    case "10km d5":
+                //    case "10km d6":
+                //    case "10km j":
+                //    case "10km j1":
+                //    case "10km j2":
+                //    case "10km j3":
+                //    case "10km j4":
+                //    case "10km j5":
+                //    case "10km j6":
+                //    case "10km m":
+                //    case "10km m1":
+                //    case "10km m2":
+                //    case "10km m3":
+                //    case "10km m4":
+                //    case "10km m5":
+                //    case "10km m6":
+                //    case "10km g":
+                //    case "10km g1":
+                //    case "10km g2":
+                //    case "10km g3":
+                //    case "10km g4":
+                //    case "10km g5":
+                //    case "10km g6":
+                //        return 10000;
+                //    case "1d5km":
+                //    case "2d5km":
+                //    case "3d5km":
+                //    case "4d5km":
+                //    case "5d5km":
+                //    case "6d5km":
+                //    case "1m5km":
+                //    case "2m5km":
+                //    case "3m5km":
+                //    case "4m5km":
+                //    case "5m5km":
+                //    case "6m5km":
+                //    case "1j5km":
+                //    case "2j5km":
+                //    case "3j5km":
+                //    case "4j5km":
+                //    case "5j5km":
+                //    case "6j5km":
+                //    case "1g5km":
+                //    case "2g5km":
+                //    case "3g5km":
+                //    case "4g5km":
+                //    case "5g5km":
+                //    case "6g5km":
+                //    case "5km d1":
+                //    case "5km d2":
+                //    case "5km d3":
+                //    case "5km d4":
+                //    case "5km d5":
+                //    case "5km d6":
+                //    case "5km j1":
+                //    case "5km j2":
+                //    case "5km j3":
+                //    case "5km j4":
+                //    case "5km j5":
+                //    case "5km j6":
+                //    case "5km g1":
+                //    case "5km g2":
+                //    case "5km g3":
+                //    case "5km g4":
+                //    case "5km g5":
+                //    case "5km g6":
+                //    case "5km m1":
+                //    case "5km m2":
+                //    case "5km m3":
+                //    case "5km m4":
+                //    case "5km m5":
+                //    case "5km m6":
+                //        return 5000;
+                //    case "trim":
+                //        return 4999;
+                //    case "2km":
+                //        return 2000;
+                //    case "500m":
+                //    case "600m":
+                //        return 600;
+                //    default:
+                //        return -1;
+                //}
+                return raceLength;
             }
         }
 
